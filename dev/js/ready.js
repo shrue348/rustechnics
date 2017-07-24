18a7ebd8978cb951cba1901d34ecb1076b373ad1
Aleksander Alekseev <shure348@iMac-Aleksander-2.local>
2017-07-24 22:00:41 +0300

    commit

---
+++ dev/js/ready.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//Рейтинги-звезды
+$(function(){
+	$('input.rating_star').rating({readOnly: true});
+	$('input.input_rating_star').rating({required: true});
+	$('input.item_rating_star').rating({readOnly: true, split: 2});
+});
+

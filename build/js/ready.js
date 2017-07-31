41a5cd7817d403ef18aa0c88f1ecf9737494971a
shrue348 <shure348@gmail.com>
2017-07-31 17:36:25 +0300

    commit

--- build/js/ready.js
+++ build/js/ready.js
@@ -1,5 +1,3 @@
-
-
 //Рейтинги-звезды
 $(function(){
 	$('input.rating_star').rating({readOnly: true});
@@ -26,5 +24,64 @@ $(function(){
 
 $(function(){
 	$('.slideshow.vertical').cycle()
+})
 
+$(function(){
+	$('[name="cart_form_phone"], [name="cart_form_phone_2"], [name="phone"]').mask("+7 (999) 999-9999")
+})
+
+
+$(function(){//Переключалка кол-ва товаров в корзину
+	$('.quantity').on('click','button', function(e){
+		var self = $(this),
+			input = self.siblings('input')[0],
+			currentValue = +input.value;
+
+		if (self.is('.quantity_plus')  ){
+			input.value = currentValue + 1;
+		} else if (currentValue == 1) {
+			return false;
+		} else {
+			input.value = currentValue - 1;
+		}
+		return false;
+	});
+})
+
+
+
+/*---Mobile Layout-----*/
+$(function(){
+	$('.m_menu-toggle').on('click', function(){
+		$('.m_header').toggleClass('menu-opened')
+	});
+
+	$('.m_overlay').on('click', function(){
+		$('.m_header').removeClass('menu-opened')
+	});
+});
+
+window.onscroll = function(){
+	$('.m_header').removeClass('menu-opened')
+};
+
+
+/*----CatMenu------*/
+$(function(){
+	var href = location.pathname;
+
+	$('.big_cat_btn').on('click', function(){
+		$(this).toggleClass('active');
+	});
+
+	$('body').on('click', function(e){
+		var div = $(".cat_menu_wrap"); 
+		if (!div.is(e.target)
+		    && div.has(e.target).length === 0) { 
+			div.find('.big_cat_btn').removeClass('active')
+		}
+	});
+
+	if(href == '/') $('.big_cat_btn').addClass('main_active')
+	
 })
